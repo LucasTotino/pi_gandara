10868108513b9c229abebd587f1efded6089f2ac
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-08-28 09:31:34 -0300

    1.0.1
    fix: atualização dos caminhos para todos os index.php

--- index.php
+++ index.php
@@ -4,39 +4,39 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="/sistema/css/style.css">
+  <link rel="stylesheet" href="/pi_gandara/css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
-  <title>Trabalho Gandara!</title>
+  <title>ERP Cítrico</title>
 </head>
 <header>
 <nav>
-    <a href="/sistema/index.php" title="Home">
+    <a href="/pi_gandara/index.php" title="Home">
       <span class="fa fa-bars" aria-hidden="true"></span>
       <span class="label">Menu</span>
     </a>
-    <a href="/sistema/estoque/index.php" title="Estoque">
+    <a href="/pi_gandara/estoque/index.php" title="Estoque">
       <span class="fa fa-solid fa-box"></span>
       <span class="label">Estoque</span>
     </a>
-    <a href="/sistema/compras/index.php" title="Compras" class="active">
+    <a href="/pi_gandara/compras/index.php" title="Compras" class="active">
       <span class="fa fa-money"></span>
       <span class="label">Compras</span>
     </a>
-    <a href="/sistema/pcp/index.php" title="PCP">
+    <a href="/pi_gandara/pcp/index.php" title="PCP">
       <span class="fa fa-helmet-safety"></span>
       <span class="label">PCP</span>
     </a>
-    <a href="/sistema/financeiro/index.php" title="Financeiro">
+    <a href="/pi_gandara/financeiro/index.php" title="Financeiro">
       <span class="fa fa-solid fa-dollar-sign"></span>
       <span class="label">Financeiro</span>
     </a>
-    <a href="/sistema/folhaPagamento/index.php" title="Folha de Pagamento">
+    <a href="/pi_gandara/folhaPagamento/index.php" title="Folha de Pagamento">
       <span class="fa fa-file-invoice-dollar"></span>
       <span class="label">Folha de Pagamento</span>
     </a>
-    <a href="/sistema/comercial/index.php" title="Comercial">
+    <a href="/pi_gandara/comercial/index.php" title="Comercial">
       <span class="fa fa-chart-line"></span>
       <span class="label">Comercial</span>
     </a>
