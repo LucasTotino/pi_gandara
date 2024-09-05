f67fd513c983f9b04011195220c76ec5f28232f8
lucastotino <lucas_totino@hotmail.com>
2024-09-05 15:45:30 -0300

    1.0.3

--- compras/cadCliente.php
+++ compras/cadCliente.php
@@ -158,9 +158,6 @@ if (isset($_POST['submit'])) {
     </main>
 
     <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-
-</body>
+    </body>
 
 </html>
