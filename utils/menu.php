68193f8cfb96a8a810c4278f295e88fc0ae85dfc
lucastotino <lucas_totino@hotmail.com>
2024-09-05 15:33:42 -0300

    1.0.2

--- utils/menu.php
+++ utils/menu.php
@@ -1,16 +1,3 @@
-<!doctype html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="/pi_gandara/css/style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-
-  <title>Trabalho Gandara!</title>
-</head>
-<header>
 <nav>
     <a href="/pi_gandara/index.php" title="Home">
       <span class="fa fa-bars" aria-hidden="true"></span>
@@ -41,16 +28,3 @@
       <span class="label">Comercial</span>
     </a>
   </nav>
-
-</header>
-<body>
-
-
-
-
-
-  <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
