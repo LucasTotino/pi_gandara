482f8a67e175b35467e8c826d6e2b4ee52dd5d7f
Thiago Mesquita <113726254+ThiagoLMesquita@users.noreply.github.com>
2024-09-11 02:38:51 +0000

    ajustes links

--- index.php
+++ index.php
@@ -25,31 +25,31 @@
   </head>
   <header>
     <nav class="">
-      <a href="/sistema/index.php" title="Home">
+      <a href="../index.php" title="Home">
         <span class="fa fa-bars" aria-hidden="true"></span>
         <span class="label">Menu</span>
       </a>
-      <a href="/sistema/estoque/index.php" title="Estoque">
+      <a href="../estoque/index.php" title="Estoque">
         <span class="fa fa-solid fa-box"></span>
         <span class="label">Estoque</span>
       </a>
-      <a href="/sistema/compras/index.php" title="Compras" class="active">
+      <a href="../compras/index.php" title="Compras" class="active">
         <span class="fa fa-money"></span>
         <span class="label">Compras</span>
       </a>
-      <a href="/sistema/pcp/index.php" title="PCP">
+      <a href="../pcp/index.php" title="PCP">
         <span class="fa fa-helmet-safety"></span>
         <span class="label">PCP</span>
       </a>
-      <a href="/sistema/financeiro/index.php" title="Financeiro">
+      <a href="../financeiro/index.php" title="Financeiro">
         <span class="fa fa-solid fa-dollar-sign"></span>
         <span class="label">Financeiro</span>
       </a>
-      <a href="/sistema/folhaPagamento/index.php" title="Folha de Pagamento">
+      <a href="../folhaPagamento/index.php" title="Folha de Pagamento">
         <span class="fa fa-file-invoice-dollar"></span>
         <span class="label">Folha de Pagamento</span>
       </a>
-      <a href="/sistema/comercial/index.php" title="Comercial">
+      <a href="../comercial/index.php" title="Comercial">
         <span class="fa fa-chart-line"></span>
         <span class="label">Comercial</span>
       </a>
