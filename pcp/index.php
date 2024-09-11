bc28f551d9607dd74da31f5368d97e08b70964c4
Thiago Mesquita <113726254+ThiagoLMesquita@users.noreply.github.com>
2024-09-11 02:32:42 +0000

    menu novo e novo pcp

--- pcp/index.php
+++ pcp/index.php
@@ -11,66 +11,118 @@
   <title>PCP</title>
 </head>
 <header>
-<!doctype html>
-<html lang="pt-br">
+  <!doctype html>
+  <html lang="pt-br">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="/sistema/css/style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/sistema/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
-  <title>Trabalho Gandara!</title>
-</head>
-<header>
-<nav>
-    <a href="/sistema/index.php" title="Home">
-      <span class="fa fa-bars" aria-hidden="true"></span>
-      <span class="label">Menu</span>
-    </a>
-    <a href="/sistema/estoque/index.php" title="Estoque">
-      <span class="fa fa-solid fa-box"></span>
-      <span class="label">Estoque</span>
-    </a>
-    <a href="/sistema/compras/index.php" title="Compras" class="active">
-      <span class="fa fa-money"></span>
-      <span class="label">Compras</span>
-    </a>
-    <a href="/sistema/pcp/index.php" title="PCP">
-      <span class="fa fa-helmet-safety"></span>
-      <span class="label">PCP</span>
-    </a>
-    <a href="/sistema/financeiro/index.php" title="Financeiro">
-      <span class="fa fa-solid fa-dollar-sign"></span>
-      <span class="label">Financeiro</span>
-    </a>
-    <a href="/sistema/folhaPagamento/index.php" title="Folha de Pagamento">
-      <span class="fa fa-file-invoice-dollar"></span>
-      <span class="label">Folha de Pagamento</span>
-    </a>
-    <a href="/sistema/comercial/index.php" title="Comercial">
-      <span class="fa fa-chart-line"></span>
-      <span class="label">Comercial</span>
-    </a>
-  </nav>
+    <title>Trabalho Gandara!</title>
+  </head>
+  <header>
+    <nav class="">
+      <a href="/sistema/index.php" title="Home">
+        <span class="fa fa-bars" aria-hidden="true"></span>
+        <span class="label">Menu</span>
+      </a>
+      <a href="/sistema/estoque/index.php" title="Estoque">
+        <span class="fa fa-solid fa-box"></span>
+        <span class="label">Estoque</span>
+      </a>
+      <a href="/sistema/compras/index.php" title="Compras" class="active">
+        <span class="fa fa-money"></span>
+        <span class="label">Compras</span>
+      </a>
+      <a href="/sistema/pcp/index.php" title="PCP">
+        <span class="fa fa-helmet-safety"></span>
+        <span class="label">PCP</span>
+      </a>
+      <a href="/sistema/financeiro/index.php" title="Financeiro">
+        <span class="fa fa-solid fa-dollar-sign"></span>
+        <span class="label">Financeiro</span>
+      </a>
+      <a href="/sistema/folhaPagamento/index.php" title="Folha de Pagamento">
+        <span class="fa fa-file-invoice-dollar"></span>
+        <span class="label">Folha de Pagamento</span>
+      </a>
+      <a href="/sistema/comercial/index.php" title="Comercial">
+        <span class="fa fa-chart-line"></span>
+        <span class="label">Comercial</span>
+      </a>
+    </nav>
+
+  </header>
+
+  <body>
+    <div class="container">
+      <div class="row p-5 justify-content-center d-flex align-items-center">
+        <a href="../pcp/planejamento.php" class="btn">
+          <div class="col-6 p-5 m-1">
+            <div style="width: 18rem;">
+              <span class="fa fa-columns fa-5x" aria-hidden="true"></span>
+              <div class="card-body">
+                <h3>Planejamento</h3>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="../index.php" class="btn">
+          <div class="col-6 p-5 m-1">
+            <div style="width: 18rem;">
+              <span class="fa fa-industry fa-5x" aria-hidden="true"></span>
+              <div class="card-body">
+                <h3>Controle de Produção</h3>
+              </div>
+            </div>
+          </div>
+        </a>
+
+      </div> <!--Fim da row -->
+
+      <div class="row p-5 justify-content-center d-flex align-items-center">
+        <a class="btn">
+          <div class="col-6 p-5 m-1">
+            <div style="width: 18rem;">
+              <span class="fa fa-check-circle fa-5x" aria-hidden="true"></span>
+              <div class="card-body">
+                <h3>Qualidade</h3>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a class="btn">
+          <div class="col-6 p-5 m-1">
+            <div style="width: 18rem;">
+              <span class="fa fa fa-truck fa-5x" aria-hidden="true"></span>
+              <div class="card-body">
+                <h3>Estoque e Logística</h3>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div> <!--Fim da row -->
+
+
+    </div>
+
+
+
+
+    <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
+
+  </body>
+
+  </html>
 
 </header>
+
 <body>
 
-
-
-
-
-  <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
-
-</body>
-
-</html>
-
-</header>
-<body>
-  
 
   <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
 
