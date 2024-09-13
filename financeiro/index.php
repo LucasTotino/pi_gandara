f1d0e536734accaa9dc57f2ee6d10767193140a7
RENAN FERNANDO BRANDI <0200782223016>
2024-09-13 18:15:13 -0300

    1.1

--- financeiro/index.php
+++ financeiro/index.php
@@ -2,127 +2,150 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">  
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="stylesheet" type="text/css" href="styles.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="/pi_gandara/css/style.css">
-  <title>Financeiro</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/pi_gandara/css/style.css">
+    <title>FINANCEIRO</title>
 </head>
 
-<body>  <!-- VERIFICAR FOTO  DO LIVRO  FINANCEIRO DO GANDARA -->
-<header>
+<body>
+    <header>
         <?php
-            include_once('../utils/menu.php');
+        include_once('../utils/menu.php');
         ?>
-  </header>
+    </header>
+    <main>
 
-      <div class="container">
+        <div class="container">
 
-         <div class="row p-5 d-flex">
+            <div class="row p-3 justify-content-center d-flex align-items-center">
 
-                <a href="razaoGeral.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-city fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3>Razão Geral</h3>
-                      </div>
+                <a href="razaoGeral.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-city fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Razão Geral</b></h4>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </a>
 
-                <a href="contasPagar.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-money-bill-transfer fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3><nobr>Contas a</nobr> Pagar</h3>
-                      </div>
+                <a href="contasPagar.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-money-bill-transfer fa-5x" aria-hidden="true"></span>
+                            <div class="card-body d-flex justify-content-center">
+                                <h4>
+                                    <b>Contas a<br> Pagar</nobr></b>
+                                </h4>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </a>
 
-                <a href="contasReceber.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-money-bill-transfer fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3><nobr>Contas a</nobr> Receber</h3>
-                      </div>
+                <a href="contasReceber.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-money-bill-transfer fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Contas a Receber</b></h3>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </a>
 
-                <a href="gerCaixa.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-scale-balanced fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3><nobr>Gerenciamento de</nobr> Caixa</h3>
-                      </div>
+
+                <a href="gerCaixa.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-scale-balanced fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Gerenciamento de Caixa</b></h4>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </a>
-
-            </div><!--  Fim da row --> 
-
-
-            <div class="row p-5 d-flex">
-
-                <a href="gerFornecedores.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-truck fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3><nobr>Gerenciamento de</nobr> Fornecedores</h3>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-
-                <a href="gerBancario.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa-solid fa-landmark fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3>Gerenciamento Bancário</h3>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-
-                <a href="gerFiscal.php" class="btn" style="width: 16rem;">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa fa-industry fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3>Gerenciamento Fiscal</h3>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-
-                <a href="relatoriosAnalises.php" class="btn" style="width: 16rem; ">
-                  <div class="col-3 p-5 m-1 d-flex">
-                    <div>
-                      <span class="fa fa-pie-chart fa-4x" aria-hidden="true"></span>
-                      <div class="card-body d-flex">
-                        <h3><nobr>Relatórios e</nobr> Análises</h3>
-                      </div>
-                    </div>
-                  </div>
                 </a>
 
             </div> <!--Fim da row -->
 
-      </div>
- 
+            <div class="row p-3 justify-content-center d-flex align-items-center">
+
+                <a href="gerFornecedores.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-truck fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Gerenciamento <nobr>de Fornecedores</b></nobr>
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="gerBancario.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa-solid fa-landmark fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Gerenciamento Bancário</b></h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="gerFiscal.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa fa-industry fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4><b>Gerenciamento Fiscal</b></h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="relatoriosAnalises.php" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 14rem;">
+                            <span class="fa fa-pie-chart fa-5x" aria-hidden="true"></span>
+                            <div class="card-body justify-content-center">
+                                <h4>
+                                    <b>Relatórios<br> e Análises</b>
+                                </h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div> <!--Fim da row -->
 
 
-  <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
+            <!-- <div class="row p-5 justify-content-center d-flex align-items-center">
+                <a href="" class="btn">
+                    <div class="col-2 p-2 m-1">
+                        <div style="width: 12rem;">
+                            <span class="fa fa-cart-shopping fa-5x" aria-hidden="true"></span>
+                            <div class="card-body">
+                                <h4>*TESTE*</h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div> -->
+            
+
+
+        </div>
+
+    </main>
+
+
+
+
+    <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
