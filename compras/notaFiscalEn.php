4302d51cda59eeea162a6b94cc9d9cc629a161d5
PEDRO LUIS QUEIROZ CABRAL <0200782223011>
2024-10-24 19:37:17 -0300

    att 24/10

--- compras/notaFiscalEn.php
+++ compras/notaFiscalEn.php
@@ -48,22 +48,22 @@ if (isset($_POST['submit'])) {
                 <form action="/onstudies/usuarios/iu_usuario.php" method="POST"><!-- Inicio Formulário -->
                     <div class="form-group">
                         <div class="form-row justify-content-center mt-2">
-                            
+
                             <div class="col-sm-2">
-                                <label for="bairro">Operação</label>
-                                <input type="text" class="form-control" id="bairro" name="bairro">
+                                <label for="operacao">Operação</label>
+                                <input type="text" class="form-control" id="operacao" name="operacao">
                             </div>
                             <div class="col-sm-2">
-                                <label for="bairro">Codigo do Produto</label>
-                                <input type="text" class="form-control" id="bairro" name="bairro">
+                                <label for="descricao">Descrição</label>
+                                <input type="text" class="form-control" id="descricao" name="descricao" autocomplete="on">
                             </div>
-                            <div class="col-sm-2">
-                                <label for="email">Descrição</label>
-                                <input type="text" class="form-control" id="email" name="email" autocomplete="on">
+                            <div class="col-sm-">
+                                <label for="unidadeconvercao">Unidade Converção</label>
+                                <input type="text" class="form-control" id="unidadeconvercao" name="unidadeconvercao" autocomplete="on">
                             </div>
-                            <div class="col-sm-2">
-                                <label for="email">Unidade Converção</label>
-                                <input type="text" class="form-control" id="email" name="email" autocomplete="on">
+                            <div class="col-sm-">
+                                <label for="quantidadeconvercao">Quantidade Converção</label>
+                                <input type="text" class="form-control" id="quantidadeconvercao" name="quantidadeconvercao" autocomplete="on">
                             </div>
                         </div>
                         <div class="row justify-content-center mt-2">
@@ -78,32 +78,53 @@ if (isset($_POST['submit'])) {
                                 </select>
                             </div>
                             <div class="col-sm-2">
-                                <label for="logradouro">Quantidade</label>
-                                <input type="text" class="form-control" id="logradouro" name="logradouro">
+                                <label for="quantidade">Quantidade</label>
+                                <input type="text" class="form-control" id="quantidade" name="quantidade">
                             </div>
                             <div class="col-sm-2">
-                                <label for="complemento">Data de Entrega</label>
-                                <input type="date" class="form-control" id="complemento" name="complemento">
+                                <label for="valorunit">Valor Unitario</label>
+                                <input type="number" class="form-control" id="valorunit" name="valorunit">
                             </div>
                             <div class="col-sm-2">
-                                <label for="numLogradouro">Unidade de Medida</label>
-                                <input type="number" class="form-control" id="numLogradouro" name="numLogradouro">
+                                <label for="valorunitconv">Valor Unitario Conv</label>
+                                <input type="number" class="form-control" id="valorunitconv" name="valorunitconv">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="unidademedida">Unidade de Medida</label>
+                                <input type="number" class="form-control" id="unidademedida" name="unidademedida">
                             </div>
                         </div>
                         <div class="row justify-content-center mt-2">
-                            <div class="form-group col-sm-5">
-                                <label for="nivel_acesso" class="text-danger font-weight-bold">Fornecedor:</label>
-                                <select class="form-control" id="nivel_acesso" name="nivel_acesso">
-                                    <option value=""> -- ESCOLHA -- </option>
-                                    <option <?= (isset($_GET['id']) && $user['nivel_acesso'] == 1) ? "selected" : null ?>
-                                        value="1">Leno</option>
-                                    <option <?= (isset($_GET['id']) && $user['nivel_acesso'] == 0) ? "selected" : null ?>
-                                        value="0">Brega</option>
-                                </select>
+                            <div class="col-sm-2">
+                                <label for="totalbruto">Total Bruto</label>
+                                <input type="text" class="form-control" id="totalbruto" name="totalbruto">
                             </div>
-                            <div class="col-sm-6">
-                                <label for="cidade">Observações</label>
-                                <input type="text" class="form-control" id="cidade" name="cidade">
+                            <div class="col-sm-2">
+                                <label for="porcdesconto">% Desconto</label>
+                                <input type="text" class="form-control" id="porcdesconto" name="porcdesconto">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="valordesc">Valor do Desconto</label>
+                                <input type="text" class="form-control" id="valordesc" name="valordesc">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="totalliquido">Total Liquido</label>
+                                <input type="text" class="form-control" id="totalliquido" name="totalliquido">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="tributo">Tributo</label>
+                                <input type="text" class="form-control" id="tributo" name="tributo">
+                            </div>
+
+                        </div>
+                        <div class="row justify-content-center mt-2">
+                            <div class="col-sm-2">
+                                <label for="despesas">Despesas</label>
+                                <input type="text" class="form-control" id="despesas" name="despesas">
+                            </div>
+                            <div class="col-sm-2">
+                                <label for="total">Total</label>
+                                <input type="text" class="form-control" id="total" name="total">
                             </div>
 
                         </div>
