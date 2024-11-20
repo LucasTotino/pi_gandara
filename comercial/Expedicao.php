45f2101fac0a3eb75cf432365cf2bc1978b00c4b
Guilherme Oliveira <guiolx2@hotmail.com>
2024-11-20 20:30:56 -0300

    Tela de expedição 0.1 (Inacabada)

---
+++ comercial/Expedicao.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/pi_gandara/css/style.css">
+
+    <title>Comercial e Faturamento</title>
+</head>
+
+
+<body>
+    <header>
+        <?php
+        include_once('../utils/menu.php');
+        ?>
+    </header>
+    <main>
+        <h1 class="text-center">COMERCIAL E FATURAMENTO</h1>
+        <div class="container">
+            <h3 class="text-center">Agendamento de expedição de produto acabado</h3>
+            <div class="card card-cds">
+
+            </div>
+    </main>
+
+    <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</body>
+
+</html>
