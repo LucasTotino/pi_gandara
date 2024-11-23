09172c7ac37dd4ec51f21d6995d40edb272f0bba
OBaio <otavio.a.baio@gmail.com>
2024-11-23 20:54:37 -0300

    modified:   pcp/qualidade.php

--- pcp/qualidade.php
+++ pcp/qualidade.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="/pi_gandara/css/style.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.css"
+    crossorigin="anonymous">
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
   <title>Trabalho Gandara!</title>
@@ -21,22 +22,22 @@
 
   <div class="container mt-5">
     <form action="" method="POST"><!-- Inicio Formulário -->
-    <div class="row">
+      <div class="row">
 
-<a href="../pcp/index.php" class="btn">
-  <div class="col-2">
-    <div style="width: 18rem;">
-      <span class="fa fa-chevron-left fa-2x p-3" aria-hidden=" true"></span>
-    </div>
-  </div>
-</a>
+        <a href="../pcp/index.php" class="btn">
+          <div class="col-2">
+            <div style="width: 18rem;">
+              <span class="fa fa-chevron-left fa-2x p-3" aria-hidden=" true"></span>
+            </div>
+          </div>
+        </a>
 
 
-<div class="col-6 m-5">
-  <h3>Resgistro de Qualidade</h3>
-</div>
+        <div class="col-6 m-5">
+          <h3>Resgistro de Qualidade</h3>
+        </div>
 
-</div>
+      </div>
 
 
       <div class="form-group">
@@ -44,11 +45,13 @@
         <div class="form-row justify-content-center mt-2">
           <div class="col-sm-6">
             <label for="areaRef">Área de Referência</label>
-            <input type="number" class="form-control" id="areaRef" name="areaRef" placeholder="Puxar um select do banco">
+            <input type="number" class="form-control" id="areaRef" name="areaRef" placeholder="Puxar um select do banco"
+              value="<?= $id ? $user['areaRef'] : '' ?>">
           </div>
           <div class="col-sm-6">
             <label for="dataMedicao">Data da medição</label>
-            <input type="date" class="form-control" id="dataMedicao" name="dataMedicao">
+            <input type="date" class="form-control" id="dataMedicao" name="dataMedicao"
+              value="<?= $id ? $user['dataMedicao'] : '' ?>">
           </div>
 
 
@@ -57,7 +60,8 @@
         <div class="form-row justify-content-center mt-2">
           <div class="col-sm-4">
             <label for="diametroMed">Diâmetro da Fruta (cm)</label>
-            <input type="number" class="form-control" id="diametroMed" name="diametroMed">
+            <input type="number" class="form-control" id="diametroMed" name="diametroMed"
+              value="<?= $id ? $user['diametroMed'] : '' ?>">
           </div>
 
 
@@ -65,34 +69,60 @@
             <label for="conformidadeVenda">Está em conformidade para Venda?</label>
             <div class="input-group">
               <select class="custom-select" id="conformidadeVenda" name="conformidadeVenda">
-                <option>Selecione</option>
-                <option>Sim</option>
-                <option>Não</option>
+                <option value="">Selecione</option>
+                <option <?= ($id && $user["conformidadeVenda"] == "Sim") ? "selected" : '' ?> value="Sim">Sim</option>
+                <option <?= ($id && $user["conformidadeVenda"] == "Não") ? "selected" : '' ?> value="Não">Não</option>
               </select>
             </div>
           </div>
 
-      </div>
-      <!-- Botões -->
-      <div class="form-row justify-content-center">
-        <div class="col-sm-3 mt-3">
-          <button type="submit" name="submit" class="btn btn-success">Cadastrar</button>
         </div>
-        <div class="col-sm-3 mt-3">
-          <button type="reset" class="btn btn-warning">Cancelar</button>
+        <!-- Botões -->
+        <div class="form-row justify-content-center">
+          <div class="col-sm-3 mt-3">
+            <button type="submit" name="submit" class="btn btn-success">Cadastrar</button>
+          </div>
+          <div class="col-sm-3 mt-3">
+            <button type="reset" class="btn btn-warning">Cancelar</button>
+          </div>
+          <div class="col-sm-3 mt-3">
+            <a href="index.php"><button type="button" class="btn btn-danger">Voltar</button></a>
+          </div>
         </div>
-        <div class="col-sm-3 mt-3">
-          <a href="index.php"><button type="button" class="btn btn-danger">Voltar</button></a>
-        </div>
-      </div>
     </form>
 
   </div>
 
-
-
-
-
+  <div class="container mt-5">
+    <h2 class="d-flex justify-content-center">Medições realizadas</h2>
+    <div class="card p-3">
+      <table class="table table-striped table-light">
+        <thead>
+          <tr>
+            <th>Área Referência</th>
+            <th>Data inspeção</th>
+            <th>Diametro Fruto</th>
+            <th>Apropriado para venda ?</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($linha = $dados->fetch_assoc()): ?>
+            <tr>
+              <td><?= $linha['areaRef'] ?></td>
+              <td><?= $linha['dataMedicao'] ?></td>
+              <td><?= $linha['diametroMed'] ?></td>
+              <td><?= $linha['conformidadeVenda'] ?></td>
+              <td>
+                <a href=".php?id=<?= $linha['id'] ?>" class="btn btn-warning btn-sm">Editar</a>
+                <button type="button" class="btn btn-danger btn-sm" data-id="<?= $linha['id'] ?>">Excluir</button>
+              </td>
+            </tr>
+          <?php endwhile; ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
   <script src="https://kit.fontawesome.com/74ecb76a40.js" crossorigin="anonymous"></script>
 
